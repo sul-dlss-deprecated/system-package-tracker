413a845e083c43b7713cfb6d5d9db2ebb614f147
Jon Robertson <jonrober@stanford.edu>
2016-02-16 09:58:42 -0800

    Keep a list of fixed versions in advisories table for debugging

--- db/migrate/20160130001617_create_advisories.rb
+++ db/migrate/20160130001617_create_advisories.rb
@@ -2,13 +2,14 @@ class CreateAdvisories < ActiveRecord::Migration
   def change
     create_table :advisories do |t|
       t.string :name
-	    t.string :description
-	    t.string :issue_date
-	    t.string :references
-	    t.string :kind
-	    t.string :synopsis
+      t.string :description
+      t.string :issue_date
+      t.string :references
+      t.string :kind
+      t.string :synopsis
       t.string :severity
       t.string :os_family
+      t.text :fix_versions
 
       t.timestamps null: false
     end
