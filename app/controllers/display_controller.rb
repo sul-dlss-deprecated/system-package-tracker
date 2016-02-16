791fae9f40e5170da8f9dae667eb344f704ec2d5
Jon Robertson <jonrober@stanford.edu>
2016-02-16 10:02:46 -0800

    display_controller: Separate out updates and advisories

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -12,7 +12,8 @@ class DisplayController < ApplicationController
 
       # Go through each package.  In some cases (gems) there may be multiple
       # versions of a package on the machine.
-      server.servers_to_packages.each do |package_map|
+      server.servers_to_packages.find_each do |package_map|
+        next unless package_map.status == 'installed'
         package = Package.find(package_map.package_id)
         name = package.name
         provider = package.provider
@@ -21,53 +22,36 @@ class DisplayController < ApplicationController
         # package.
         if !@report[server.hostname].key?(provider)
           @report[server.hostname][provider] = {}
-          @report[server.hostname][provider][name] = {}
-          @report[server.hostname][provider][name]['version'] = []
+          @report[server.hostname][provider][name] = []
         elsif !@report[server.hostname][provider].key?(name)
-          @report[server.hostname][provider][name] = {}
-          @report[server.hostname][provider][name]['version'] = []
+          @report[server.hostname][provider][name] = []
         end
 
         # Add the version.
-        @report[server.hostname][provider][name]['version'] << package.version
-
-        # See if there are any advisories for this package.
-        package.advisories_to_packages.each do |advisory_map|
-          advisory = Advisory.find(advisory_map.advisory_id)
-          unless @report[server.hostname][provider][name].key?('advisories')
-            @report[server.hostname][provider][name]['advisories'] = []
-          end
-          advisory_report = {}
-          advisory_report['name'] = advisory.name
-          advisory_report['synopsis'] = advisory.synopsis
-          advisory_report['issue_date'] = advisory.issue_date
-          advisory_report['kind'] = advisory.kind
-          advisory_report['severity'] = advisory.severity
-          @report[server.hostname][provider][name]['advisories'] << advisory_report
-        end
+        @report[server.hostname][provider][name] << package.version
       end
     end
   end
 
-  # TODO: Flesh this out.
-  # Create a report on all servers that have pending updates.  This should
-  # show the servers with pending updates, the names and versions of those
-  # files, and any advisories linked to those updates.
-  def updates
+  # Create a report on all servers that have advisories.  This should show
+  # the servers with advisories, the names and versions of the affected
+  # packages, and the version required to fix the advisory.
+  def advisories
     @report = {}
     Server.find_each do |server|
 
       # Go through each package.  In some cases (gems) there may be multiple
       # versions of a package on the machine.
       packages = {}
-      server.servers_to_packages.each do |package_map|
+      server.servers_to_packages.find_each do |package_map|
+        next unless package_map.status == 'pending'
         package = Package.find(package_map.package_id)
         name = package.name
         provider = package.provider
 
         # See if there are any advisories for this package.
         advisories = []
-        package.advisories_to_packages.each do |advisory_map|
+        package.advisories_to_packages.find_each do |advisory_map|
           advisory = Advisory.find(advisory_map.advisory_id)
 
           advisory_report = {}
@@ -76,6 +60,18 @@ class DisplayController < ApplicationController
           advisory_report['issue_date'] = advisory.issue_date
           advisory_report['kind'] = advisory.kind
           advisory_report['severity'] = advisory.severity
+
+          # Filter out to show only the fixed packages that match the current
+          # package.
+          fixed = []
+          advisory.fix_versions.split("\n").each do |fixed_package|
+            m = /^(.+)-([^-]+)-([^-]+)\.(\w+)\.rpm$/.match(fixed_package)
+            next unless m[1] == name
+            next unless m[4] == package.arch
+            fixed.push(m[2] + '-' + m[3])
+          end
+          advisory_report['fix_versions'] = fixed.join("\n")
+
           advisories << advisory_report
         end
 
@@ -85,6 +81,35 @@ class DisplayController < ApplicationController
           end
           packages[package.name][package.version] = advisories
         end
+      end
+      unless packages.empty?
+        @report[server.hostname] = packages
+      end
+    end
+  end
+
+  # Create a report on all servers that have pending updates.  This should
+  # show the servers with pending updates and the names and versions of those
+  # files.
+  def updates
+    @report = {}
+    Server.find_each do |server|
+
+      # Go through each package.  In some cases (gems) there may be multiple
+      # versions of a package on the machine.
+      packages = {}
+      server.servers_to_packages.find_each do |package_map|
+        next unless package_map.status == 'pending'
+        package = Package.find(package_map.package_id)
+
+        if !packages.key?(package.name)
+          packages[package.name] = []
+        end
+
+        new = {}
+        new['provider'] = package.provider
+        new['version'] = package.version
+        packages[package.name] << new
       end
       unless packages.empty?
         @report[server.hostname] = packages
