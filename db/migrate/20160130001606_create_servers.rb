43a10fb0068e6e20b17fda108bb897cebf665218
Jon Robertson <jonrober@stanford.edu>
2016-02-16 09:57:58 -0800

    Add a server checkin time to the servers table

--- db/migrate/20160130001606_create_servers.rb
+++ db/migrate/20160130001606_create_servers.rb
@@ -4,6 +4,7 @@ class CreateServers < ActiveRecord::Migration
       t.string :hostname
       t.string :os_family
       t.string :os_release
+      t.datetime :last_checkin
 
       t.timestamps null: false
     end
