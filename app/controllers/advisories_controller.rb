4fbfc60ae29b10cec940f0b351b7a4a9a08f6f9b
Jon Robertson <jonrober@stanford.edu>
2016-02-18 12:51:12 -0800

    Add tests for advisories controller, with code refactors

--- app/controllers/advisories_controller.rb
+++ app/controllers/advisories_controller.rb
@@ -12,72 +12,87 @@ class AdvisoriesController < ApplicationController
   # either Gemnasium or https://github.com/rubysec/ruby-advisory-db for
   # getting advisories on gems, then add a second function for that.
 
-  def index
-
-    # This URL posts CentOS errata for Spacewalk, by parsing the
-    # CentOS-Announce archives.  If this ever stops being maintained, then
-    # we would need to look at another source/using his scripts for ourselves.
+  # This URL posts CentOS errata for Spacewalk, by parsing the
+  # CentOS-Announce archives.  If this ever stops being maintained, then
+  # we would need to look at another source/using his scripts for ourselves.
+  def get_centos_advisories
     url = 'http://cefs.steve-meier.de/errata.latest.xml'
     xml_data = Net::HTTP.get_response(URI.parse(url)).body
+  end
+
+  # Add an adivory to the database.
+  def add_advisory (advisory)
+
+    # TODO: Just return the record if the advisory already exists.
+
+    # Many advisories don't have a set severity, so give a default.
+    if advisory.attributes['severity']
+      severity = advisory.attributes['severity']
+    else
+      severity = 'Unknown'
+    end
+
+    # Get all the package names at once to save as details.  We're going to
+    # go through them again later, but do this once now to save them with
+    # the normal record.  This field will only be to keep the information
+    # with the record for manual debugging.
+    packages = []
+    advisory.elements.each('packages') do |adv_package|
+      packages.push(adv_package.text)
+    end
+
+    attributes = advisory.attributes
+    adv = Advisory.find_or_create_by(name: advisory.name,
+                                     description: attributes['description'],
+                                     issue_date: attributes['issue_date'],
+                                     references: attributes['references'],
+                                     kind: attributes['type'],
+                                     synopsis: attributes['synopsis'],
+                                     severity: severity,
+                                     os_family: 'centos',
+                                     fix_versions: packages.join("\n"))
+    return adv
+  end
+
+  # Take a single package name that has a yum advisory filed against it, then
+  # parse out that name and find any packages with that name.  Check to see
+  # which ones are before the patched version and mark any of those packages
+  # as falling under the advisory.
+  def check_yum_package (adv, advisory_package)
+    m = /^(.+)-([^-]+)-([^-]+)\.(\w+)\.rpm$/.match(advisory_package)
+    package_name = m[1]
+    package_version = m[2]
+    package_subver = m[3]
+    package_architecture = m[4]
+    return nil if package_architecture == 'src'
+
+    advisory_ver = RPM::Version.new(package_version + '-' + package_subver)
+    Package.where(name: package_name, arch: package_architecture,
+                  provider: 'yum').find_each do |package|
+
+      check_ver = RPM::Version.new(package.version)
+      if (advisory_ver.newer?(check_ver))
+        adv.advisories_to_packages.create(package_id: package.id)
+      end
+    end
+  end
+
+  # Main action.  Look up the advisories list, then iterate through all
+  # advisories to add and associate them with our packages.
+  def index
 
     # Parse the data and look up.  The file is formatted with every advisory
     # under <opt>.
+    xml_data = get_centos_advisories
     doc = REXML::Document.new(xml_data)
     doc.elements.each('opt/*') do |advisory|
 
-      # Skip the meta item, the one thing in the XML doc that's not an advisory.
+      # Skip the meta item, the one thing in the XML that's not an advisory.
       next if advisory.name == 'meta'
 
-      # Many advisories don't have a set severity, so give a default.
-      if advisory.attributes['severity']
-        severity = advisory.attributes['severity']
-      else
-        severity = 'Unknown'
-      end
-
-      # Get all the package names at once to save as details.  We're going to
-      # go through them again later, but do this once now to save them with
-      # the normal record.  This field will only be to keep the information
-      # with the record for manual debugging.
-      packages = []
+      adv = add_advisory(advisory)
       advisory.elements.each('packages') do |adv_package|
-        packages.push(adv_package.text)
-      end
-
-      # Create the advisory in the database if it does not yet exist.
-      adv = Advisory.find_or_create_by(name: advisory.name,
-                                       description: advisory.attributes['description'],
-                                       issue_date: advisory.attributes['issue_date'],
-                                       references: advisory.attributes['references'],
-                                       kind: advisory.attributes['type'],
-                                       synopsis: advisory.attributes['synopsis'],
-                                       severity: severity,
-                                       os_family: 'centos',
-                                       fix_versions: packages.join("\n"))
-
-      # Now link the advisory to any known packages.  We don't bother removing
-      # old entries, since an advisory that affects version X should always
-      # affect version X.  Each advisory can affect multiple packages.
-      advisory.elements.each('packages') do |adv_package|
-        m = /^(.+)-([^-]+)-([^-]+)\.(\w+)\.rpm$/.match(adv_package.text)
-        package_name = m[1]
-        package_version = m[2]
-        package_subver = m[3]
-        package_architecture = m[4]
-        next if package_architecture == 'src'
-
-        Package.where(name: package_name, arch: package_architecture,
-                      provider: 'yum').find_each do |package|
-
-          # Match the package's version against the version in the advisory,
-          # associating the package and advisory only if the advisory needs
-          # a later version of the package than this.
-          check_ver = RPM::Version.new(package.version)
-          advisory_ver = RPM::Version.new(package_version + '-' + package_subver)
-          if (advisory_ver.newer?(check_ver))
-            adv.advisories_to_packages.create(package_id: package.id)
-          end
-        end
+        check_yum_package(adv, adv_package.text)
       end
     end
   end
--- spec/controllers/advisories_controller_spec.rb
+++ spec/controllers/advisories_controller_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe AdvisoriesController, type: :controller do
+  describe '#save_advisory' do
+    it 'fetch centos advisories' do
+      xml_data = AdvisoriesController.new.get_centos_advisories()
+      expect(xml_data).to match(/^<opt>/)
+    end
+  end
+
+  describe '#index' do
+    fixtures :packages
+
+    it 'load known data and check state' do
+      advisories = AdvisoriesController.new
+
+      # Set up a known subset of the advisories XML.
+      #advisories = instance_double('AdvisoriesController')
+      xml = '<opt><CESA-2012--0370 description="Not available" from="centos-announce@centos.org" issue_date="2012-03-07 23:16:09" multirelease="1" notes="Not available" product="CentOS Linux" references="https://rhn.redhat.com/errata/RHSA-2012-0370.html http://lists.centos.org/pipermail/centos-announce/2012-March/018479.html" release="1" severity="Important" solution="Not available" synopsis="Important CentOS xen security and bug fix update" topic="Not available" type="Security Advisory"><os_arch>i386</os_arch><os_arch>x86_64</os_arch><os_release>5</os_release><packages>xen-3.0.3-135.el5_8.2.i386.rpm</packages><packages>xen-3.0.3-135.el5_8.2.src.rpm</packages><packages>xen-3.0.3-135.el5_8.2.x86_64.rpm</packages><packages>xen-devel-3.0.3-135.el5_8.2.i386.rpm</packages><packages>xen-devel-3.0.3-135.el5_8.2.x86_64.rpm</packages><packages>xen-libs-3.0.3-135.el5_8.2.i386.rpm</packages><packages>xen-libs-3.0.3-135.el5_8.2.x86_64.rpm</packages></CESA-2012--0370><meta><author>Steve Meier</author><license>Free for non-commercial use</license><disclaimer>This software is provided AS IS. There are no guarantees. It might kill your cat.</disclaimer><timestamp>Thu Feb 11 09:08:36 UTC 2016</timestamp></meta></opt>'
+      allow(advisories).to receive(:get_centos_advisories).and_return(xml)
+
+      advisories.index
+
+      # TODO: Check to make sure the package is the correct one.
+
+      # Make sure advisory exists and has only the one correct package.
+      adv = Advisory.find_by(name: 'CESA-2012--0370')
+      expect(adv.name).to eq('CESA-2012--0370')
+      expect(adv.advisories_to_packages.size).to eq(1)
+    end
+  end
 
 end
