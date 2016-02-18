76fd04dcd7756c8aabcfc2230577528f3cbf1491
Jon Robertson <jonrober@stanford.edu>
2016-02-18 12:51:31 -0800

    Add tests for import controller, with code refactors

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -9,6 +9,21 @@
 class ImportController < ApplicationController
   require 'yaml'
 
+  # Take a hostname, os release, and the time a yaml report was generated.
+  # Use these to create or update a server record, along with clearing all
+  # packages for that record so that new packages may be updated.
+  def save_server (hostname, os_release, last_checkin)
+    server = Server.find_or_create_by(hostname: hostname)
+    server.os_release = os_release
+    server.last_checkin = last_checkin
+
+    server.servers_to_packages.clear
+    server.save
+
+    return server
+  end
+
+
   def index
     package_types = %w(yum gem)
     status_types = %w(installed pending)
@@ -16,32 +31,28 @@ class ImportController < ApplicationController
     Dir.glob('/var/lib/package-reports/*.yaml').each do |yaml_file|
       server_yaml = YAML.load(File.open(yaml_file))
 
-      # Get the host record and then clear any existing packages.
+      # Get or create a host record.
       hostname = server_yaml['system']['hostname']
-      server = Server.find_or_create_by(hostname: hostname)
-      server.servers_to_packages.clear
-      server.os_release = server_yaml['system']['release']
-      server.last_checkin = server_yaml['system']['lastrun']
-      server.save
+      server = save_server(hostname, server_yaml['system']['release'],
+	                       server_yaml['system']['lastrun'])
 
       # Go through the yaml file for a server, adding any missing packages to
       # the database and then associating them with the server.  Packages may
       # be marked either installed or pending (for upgrades not installed).
       package_types.each do |type|
         status_types.each do |status|
-          if server_yaml[type].key?(status)
-            server_yaml[type][status].each_key do |pkg|
-              arch = server_yaml[type][status][pkg]['arch'] || 'none'
+          next unless server_yaml[type].key?(status)
+          server_yaml[type][status].each_key do |pkg|
+            arch = server_yaml[type][status][pkg]['arch'] || 'none'
 
-              # Go through each package version (gem can have multiple) to add
-              # and associate.
-              server_yaml[type][status][pkg]['version'].each do |version|
-                p = Package.find_or_create_by(name: pkg, version: version,
-                                              arch: arch, provider: type)
+            # Go through each package version (gem can have multiple) to add
+            # and associate.
+            server_yaml[type][status][pkg]['version'].each do |version|
+              p = Package.find_or_create_by(name: pkg, version: version,
+                                            arch: arch, provider: type)
 
-                p.servers_to_packages.create(server_id: server.id,
-                                             status: status)
-              end
+              p.servers_to_packages.create(server_id: server.id,
+                                           status: status)
             end
           end
         end
--- spec/controllers/import_controller_spec.rb
+++ spec/controllers/import_controller_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe ImportController, type: :controller do
+  describe '#save_server' do
+    it 'adds new host to db' do
+      host = 'testhost.stanford.edu'
+	  os_release = 'centos6'
+	  report_time = '2016-01-01 01:23:45'
+	  server = ImportController.new.save_server(host, os_release, report_time)
+
+      expect(server.hostname).to eq(host)
+      expect(server.os_release).to eq(os_release)
+      expect(server.last_checkin).to eq(report_time)
+	end
+    it 'updates existing host in db' do
+
+      # TODO: Add associations to packages and test that they're removed.
+
+      host = 'testhost.stanford.edu'
+	  os_release = 'centos7'
+	  report_time = '2016-02-02 01:23:45'
+	  server = ImportController.new.save_server(host, os_release, report_time)
+
+      expect(server.hostname).to eq(host)
+      expect(server.os_release).to eq(os_release)
+      expect(server.last_checkin).to eq(report_time)
+	end
+  end
 
 end
