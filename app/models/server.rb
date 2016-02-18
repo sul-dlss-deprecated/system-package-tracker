6d4cb3415c02d4710d4d4e33f024f9ba530f642e
Jon Robertson <jonrober@stanford.edu>
2016-02-18 12:53:14 -0800

    Add associations to get only pending/installed packages

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,4 +1,10 @@
 class Server < ActiveRecord::Base
   has_many :servers_to_packages, class_name: 'ServerToPackage'
   has_many :packages, through: :servers_to_packages
+
+  has_many :servers_to_pending_packages, -> {where status: 'pending'}, class_name: 'ServerToPackage'
+  has_many :pending_packages, through: :servers_to_pending_packages, class_name: 'Package', source: :package
+
+  has_many :servers_to_installed_packages, -> {where status: 'installed'}, class_name: 'ServerToPackage'
+  has_many :installed_packages, through: :servers_to_installed_packages, class_name: 'Package', source: :package
 end
