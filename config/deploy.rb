6819ce5ec64e65c9b329d9da8001256349f7c1d0
Jon Robertson <jonrober@stanford.edu>
2018-01-24 10:23:57 -0800

    Update module versions

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.8.1'
+lock '3.10.1'
 
 set :application, 'server-reports'
 set :repo_url, 'http://github.com/sul-dlss/system-package-tracker.git'
